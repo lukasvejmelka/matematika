c2d230536c071f919974dacfc839d899c02fdc57
lukasvejmelka <lukas.vejmelka@gmail.com>
2025-12-13 20:05:58 +0100

    Add files via upload

---
+++ tasks-data.js
@@ -0,0 +1,87 @@
+  const problemsCount = {
+        "vzor": 127,
+        "ilustr": 25,
+        "16j": 25,
+        "16p": 25,
+        "17j": 24,
+        "17p": 26,
+        "18j": 23,
+        "18p": 25,
+        "19j": 26,
+        "19p": 24,
+        "20j": 25,
+        "20p": 25,
+        "21j": 26,
+        "21jm": 25,
+        "21p": 26,
+        "22j": 24,
+        "22p": 24,
+        "23j": 23,
+        "23p": 24,
+        "24j": 25,
+        "24p": 25,
+        "25j": 24,
+        "25p": 24,
+    };
+
+ const topics = {
+  "ciselne-obory-procenta": {
+    "25p": [1, 16], // Číselné obory a procenta
+    "25j": [3] //Číselné obory, procenta
+  },
+
+  "algebraicke-vyrazy": {
+    "25p": [2, 4, 5], // Algebraické výrazy a soustavy
+    "25j": [2,4,5] //Výrazy
+  },
+
+  "rovnice-nerovnice-soustavy": {
+    "25p": [3, 6, 7, 8, 9, 17], // Rovnice a nerovnice
+    "25j": [1,6,23,24] //Rovnice, nerovnice, soustavy
+  },
+
+  "funkce": {
+    "25p": [10,23], // Funkce
+    "25j": [8,9] //Funkce
+  },
+
+  "posloupnosti": {
+    "25p": [12], // Posloupnosti
+    "25j": [12,21,22] //Posloupnosti
+  },
+
+  "financni-matematika": {
+    "25p": [15], // Finanční matematika
+    "25j": [] //Finanční matematika
+  },
+
+  "planimetrie": {
+    "25p": [19,20], // Planimetrie
+"25j": [13,17,18] //Planimetrie
+  },
+
+  "stereometrie": {
+    "25p": [13,24], // Stereometrie
+    "25j": [19,20] //Stereometrie
+  },
+
+  "analyticka-geometrie": {
+    "25p": [11, 18], // Analytická geometrie
+    "25j": [10,11,16] //Analytická geometrie
+  },
+
+  "kombinatorika": {
+    "25p": [21], // Kombinatorika
+    "25j": [15] //Kombinatorika
+  },
+
+  "pravdepodobnost": {
+    "25p": [22], // Pravděpodobnost
+    "25j": [14] //Pravděpodobnost
+  },
+
+  "statistika": {
+    "25p": [14], // Statistika
+    "25j": [7] //Statistika
+  }
+};
